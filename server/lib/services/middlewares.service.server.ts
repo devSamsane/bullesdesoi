cc347e8d31e82eec2f4c7c9536d9b831e4b40525
devSamsane <>
2018-03-21 00:42:42 +0100

    chore(middlewares): initialisation des middlewares express

---
+++ server/lib/services/middlewares.service.server.ts
@@ -0,0 +1,8 @@
+import * as express from 'express';
+import { Request, Response, NextFunction } from 'express';
+
+import { properties } from '../../config/index';
+
+export class Middlewares {
+
+}
