3c582ed6032532bdf7727bf56c18706bb6672f9a
devSamsane <>
2018-03-22 20:37:13 +0100

    chore: init client configuration

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bds-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Bulles de soi';
+}
