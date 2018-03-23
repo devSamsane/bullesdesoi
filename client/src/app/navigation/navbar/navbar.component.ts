3bc318fd551290797a537c19360a3c71c28ff064
devSamsane <LAVAUX-09540@cnamts.local>
2018-03-23 10:17:19 +0100

    feat(sidenav): mise en place toggle sidenav

--- client/src/app/navigation/navbar/navbar.component.ts
+++ client/src/app/navigation/navbar/navbar.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bds-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
- toggleIconClass = true;
+export class NavbarComponent implements OnInit {
+
+  @Output() isSidenavToggle = new EventEmitter<void>();
+  toggleIconClass: boolean;
 
   toggleIcon() {
     this.toggleIconClass = !this.toggleIconClass;
+  }
+
+  toggleSidenav() {
+    this.isSidenavToggle.emit();
+  }
+
+  ngOnInit() {
+    this.toggleIconClass = false;
   }
 }
