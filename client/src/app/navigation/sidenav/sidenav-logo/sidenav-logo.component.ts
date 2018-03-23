f91fd5d03723847ef156e629eeb3bc69cea14796
devSamsane <LAVAUX-09540@cnamts.local>
2018-03-23 17:57:02 +0100

    feat(sidenav): initialisation logo container

---
+++ client/src/app/navigation/sidenav/sidenav-logo/sidenav-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bds-sidenav-logo',
+  templateUrl: './sidenav-logo.component.html',
+  styleUrls: ['./sidenav-logo.component.scss']
+})
+export class SidenavLogoComponent {
+
+}
