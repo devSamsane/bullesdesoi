85a7f3719d65a89064b8b090e4ecbe28046b6536
devSamsane <>
2018-03-24 11:55:29 +0100

    feat(auth): initialisation des compenents signin et signup

---
+++ client/src/app/authentication/signin/signin.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bds-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss']
+})
+export class SigninComponent { }
