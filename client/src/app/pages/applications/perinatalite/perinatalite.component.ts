dbd3b7179e599a6baa0c8b12ec3fa2167d1525ba
devSamsane <>
2018-03-24 11:50:32 +0100

    feat(pages): initialisation des pages statiques

---
+++ client/src/app/pages/applications/perinatalite/perinatalite.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bds-perinatalite',
+  templateUrl: './perinatalite.component.html',
+  styleUrls: ['./perinatalite.component.scss']
+})
+export class PerinataliteComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
