648f795f0c4f1e40ff4fbbda2e169784976e854d
devSamsane <>
2018-03-25 17:38:43 +0200

    feat(router-outlet): ajout margin en fonction de la route

--- client/src/app/navigation/sidenav/sidenav.component.ts
+++ client/src/app/navigation/sidenav/sidenav.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Params, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Params, ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
-import { Observable } from 'rxjs/Observable';
+// tslint:disable-next-line:import-blacklist
+import { Subject, Observable } from 'rxjs';
 import { combineLatest } from 'rxjs/observable/combineLatest';
+import { takeUntil } from 'rxjs/operators/takeUntil';
 
 import { NavigationItems } from './sidenav-menu/nav-items';
 
@@ -12,15 +14,28 @@ import { NavigationItems } from './sidenav-menu/nav-items';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
 
   params: Observable<Params>;
+  private destroyed = new Subject();
+  public url: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    this.router.events.pipe(takeUntil(this.destroyed)).subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const rootUrl = router.url.split('#')[0];
+        this.url = rootUrl;
+      }
+    });
+  }
 
   ngOnInit() {
     this.params = combineLatest(
       this.activatedRoute.pathFromRoot.map(route => route.params), Object.assign
     );
+  }
+
+  ngOnDestroy() {
+    this.destroyed.unsubscribe();
   }
 }
