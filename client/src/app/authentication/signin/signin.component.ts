b22307d1ef6f66a788225f3d34a630471275b063
devSamsane <>
2018-04-11 21:48:05 +0200

    feat(signin): ajout du formulaire

--- client/src/app/authentication/signin/signin.component.ts
+++ client/src/app/authentication/signin/signin.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bds-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent { }
+export class SigninComponent implements OnInit {
+  signinForm: FormGroup;
+
+  ngOnInit() {
+    this.signinForm = new FormGroup({
+      email: new FormControl('', { validators: [Validators.required, Validators.email]}),
+      password: new FormControl('', { validators: [Validators.required] })
+    });
+  }
+
+ }
